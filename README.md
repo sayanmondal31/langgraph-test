dc3e724a4f7890bf0134c1c404221e20e113d61a
sayanmondal31 <sayanm816@gmail.com>
2025-03-29 15:56:59 +0530

    test langchain project

---
+++ README.md
@@ -0,0 +1,26 @@
+# langgraph-test
+
+To install dependencies:
+
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run index.js
+```
+
+Add to .env file
+```
+OPENAI_API_KEY=<ENTER YOUR API KEY>
+
+LANGSMITH_TRACING=true
+LANGSMITH_ENDPOINT="https://api.smith.langchain.com"
+LANGSMITH_API_KEY=<Add your LANGSMITH_API_KEY>
+LANGSMITH_PROJECT=<ADD YOUR PROJECT NAME>
+```
+
+This project was created using `bun init` in bun v1.1.31. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
---
+++ index.js
@@ -0,0 +1,134 @@
+import { tool } from "@langchain/core/tools";
+import { z } from "zod";
+import { MessagesAnnotation, StateGraph } from "@langchain/langgraph";
+import { SystemMessage, ToolMessage } from "@langchain/core/messages";
+import { ChatOpenAI } from "@langchain/openai";
+
+const llm = new ChatOpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+  model: "gpt-4o",
+});
+
+// Define tools
+const multiply = tool(
+  async ({ a, b }) => {
+    return `${a * b}`;
+  },
+  {
+    name: "multiply",
+    description: "Multiply two numbers together",
+    schema: z.object({
+      a: z.number().describe("first number"),
+      b: z.number().describe("second number"),
+    }),
+  }
+);
+
+const add = tool(
+  async ({ a, b }) => {
+    return `${a + b}`;
+  },
+  {
+    name: "add",
+    description: "Add two numbers together",
+    schema: z.object({
+      a: z.number().describe("first number"),
+      b: z.number().describe("second number"),
+    }),
+  }
+);
+
+const divide = tool(
+  async ({ a, b }) => {
+    return `${a / b}`;
+  },
+  {
+    name: "divide",
+    description: "Divide two numbers",
+    schema: z.object({
+      a: z.number().describe("first number"),
+      b: z.number().describe("second number"),
+    }),
+  }
+);
+
+// Augment the LLM with tools
+const tools = [add, multiply, divide];
+const toolsByName = Object.fromEntries(tools.map((tool) => [tool.name, tool]));
+const llmWithTools = llm.bindTools(tools);
+
+// Nodes
+async function llmCall(state) {
+  // LLM decides whether to call a tool or not
+  const result = await llmWithTools.invoke([
+    {
+      role: "system",
+      content:
+        "You are a helpful assistant tasked with performing arithmetic on a set of inputs.",
+    },
+    ...state.messages,
+  ]);
+
+  return {
+    messages: [result],
+  };
+}
+
+async function toolNode(state) {
+  // Performs the tool call
+  const results = [];
+  const lastMessage = state.messages.at(-1);
+
+  if (lastMessage?.tool_calls?.length) {
+    for (const toolCall of lastMessage.tool_calls) {
+      const tool = toolsByName[toolCall.name];
+      const observation = await tool.invoke(toolCall.args);
+      results.push(
+        new ToolMessage({
+          content: observation,
+          tool_call_id: toolCall.id,
+        })
+      );
+    }
+  }
+
+  return { messages: results };
+}
+
+// Conditional edge function to route to the tool node or end
+function shouldContinue(state) {
+  const messages = state.messages;
+  const lastMessage = messages.at(-1);
+
+  // If the LLM makes a tool call, then perform an action
+  if (lastMessage?.tool_calls?.length) {
+    return "Action";
+  }
+  // Otherwise, we stop (reply to the user)
+  return "__end__";
+}
+
+// Build workflow
+const agentBuilder = new StateGraph(MessagesAnnotation)
+  .addNode("llmCall", llmCall)
+  .addNode("tools", toolNode)
+  // Add edges to connect nodes
+  .addEdge("__start__", "llmCall")
+  .addConditionalEdges("llmCall", shouldContinue, {
+    // Name returned by shouldContinue : Name of next node to visit
+    Action: "tools",
+    __end__: "__end__",
+  })
+  .addEdge("tools", "llmCall")
+  .compile();
+
+// Invoke
+const messages = [
+  {
+    role: "user",
+    content:
+      "Add 3 and 4 then multiply that by 10 and divide it by 2 and again multiply it by 52020 and the divide by 0",
+  },
+];
+const result = await agentBuilder.invoke({ messages });
+console.log(result.messages);
